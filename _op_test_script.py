b150ec1004b3e2c9625419261298e9f61f03a2b0
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-03-06 22:57:13 -0500

    Designed a loop to specifically parse the postview data of every simulation state. Currently working out kinks. Will proceed with visualization and analysis.

--- _op_test_script.py
+++ _op_test_script.py
@@ -14,6 +14,52 @@ target_file, raw_data = _browse_data_file()
 # split raw daat into lines
 line_data = raw_data.split('\n')
 
+# using dictionaries
+data = {}
+data['state'] = {}
+
+# find all states and subsequent data
+state_flag = 0
+nodes_flag = 0
+num_lines = len(line_data)
+for i in range(0, num_lines):
+
+    # nodal data collection
+    if nodes_flag == 1:
+        split_data = line_data[i].split(',')
+        if len(split_data) > 1:
+            data['state'][state]['nodes']['number'].append(split_data[0])
+            data['state'][state]['nodes']['xcoord'].append(split_data[1])
+            data['state'][state]['nodes']['ycoord'].append(split_data[2])
+            data['state'][state]['nodes']['zcoord'].append(split_data[3][:-1])
+        else:
+            pass
+    
+    if line_data[i][0:6] == "*STATE":
+        state_flag = 1
+        state = line_data[i][7:-1]
+        print "STATE " + state + " Found"
+        data['state'][state] = {}
+    # Adding time
+    elif line_data[i][0:11] == "*TIME_VALUE":
+        time_value = line_data[i][12:-1]
+        print "TIME_VALUE " + time_value
+        data['state'][state]['time'] = time_value
+    # Adding nodal values
+    elif line_data[i][0:6] == "*NODES" and state_flag == 1:
+        nodes_flag = 1
+        elements_flag = 0
+        data['state'][state]['nodes'] = {}
+        data['state'][state]['nodes']['number'] = []
+        data['state'][state]['nodes']['xcoord'] = []
+        data['state'][state]['nodes']['ycoord'] = []
+        data['state'][state]['nodes']['zcoord'] = []
+        
+    elif line_data[i][0:13] == "*ELEMENT_DATA":
+        nodes_flag = 0
+
+
+"""
 # find nodes
 ## find where the nodes section starts
 Nlines = len(line_data)
@@ -38,5 +84,5 @@ for i in range(0, Nindeces):
         nodes_stop_indeces.append(elements_start_indeces[i]-1)
     else:
         nodes_stop_indeces.append(nodes_start_indeces[i]+nodes_stop_indeces[0]-2)
-
+"""
 
